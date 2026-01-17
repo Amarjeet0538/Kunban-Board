addb3d81232545824018a718fac59a6c2510681d
amarjeet0538@ <amarjeet0538@gmail.com>
2026-01-17 18:57:53 +0530

    add frontend url to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,11 +8,11 @@ const app = express();
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://kunban-board-five.vercel.app",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
-	})
+	}),
 );
 
 app.use(express.json());
